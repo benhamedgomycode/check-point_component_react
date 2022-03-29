bf54b0637188fd7983c8319fd4b79b0ae3674d3f
“benhamedgomycode” <benhamedgomycode@gmail.com>
2022-03-29 00:01:28 +0000

    first commit

---
+++ src/Components/Profile/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div>
+         <p>bab souika , Tunis, Tunisia</p>   
+        </div>
+    )
+}
+
+export default Adress
---
+++ src/Components/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullName = () => {
+    return (
+        <div>
+          <p>My full name is zied ben hamed</p>  
+        </div>
+    )
+}
+
+export default FullName
